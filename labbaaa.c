8588a8b713263bbbd119721c5514284d1adf798a
Kukovich <69118658+Kukovich@users.noreply.github.com>
2022-12-21 10:03:35 +0300

    Add files via upload

---
+++ labbaaa.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include<math.h>
+double free(double a, double b, int h)
+{
+	double c;
+	int h = 
+	c = sqrt(pow(a, 2)+pow(b, 2)-2*a*b*cos(h);
+	return c;
+}
+
+int main()
+{
+	double a, b, c;
+	int h;
+	printf("\nВведите значение первой стороны = ");
+	scanf("%lf", &a);
+	printf("\nВведите значение второй стороны = ");
+	scanf("%lf", &b);
+	printf("\nВведите значение угла в радианах = ");
+	scanf("%d", &h);
+	if(a > 0 && b > 0 && 3,14159 < h)
+	{
+	c = free(a, b, h);
+	print("\nТретья сторона = %d", c);
+	return 0
+}
